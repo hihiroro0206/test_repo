0f086879a4166a00f6eb4b0a7748437583b61f12
末若比呂 <hihiroro0206@gmail.com>
2020-12-20 16:22:00 +0900

    コマンド出力to変数

--- pythonfiles/test.py
+++ pythonfiles/test.py
@@ -1,8 +1,9 @@
 print("Hello World!")
 
 import subprocess
+from subprocess import PIPE
+checkResult = subprocess.run(['tower-cli','--help'],shell=True, stdout=PIPE, stderr=PIPE, text=True)
 
-checkResult = subprocess.run(['tower-cli','--help'],shell=True)
-#print(checkResult)
-
-
+cr = checkResult.stdout
+print('STDOUT: {}'.format(cr))
+# STDOUT: 2019年 12月19日 木曜日 11時46分14秒 JST
